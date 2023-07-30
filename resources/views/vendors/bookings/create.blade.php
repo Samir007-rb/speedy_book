ef78381bb88e649946623cff13e9c7caaf401202
Samir Ranabhat <baniyaanil1203@gmail.com>
2023-07-30 17:33:16 +0545

    final code

---
+++ resources/views/vendors/bookings/create.blade.php
@@ -0,0 +1,8 @@
+@extends('templates.create')
+@push('styles')
+@endpush
+@section('form_content')
+    @include('vendors.bookings.form')
+@endsection
+@push('scripts')
+@endpush
---
+++ resources/views/vendors/bookings/edit.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.edit')
+@push('styles')
+@endpush
+@section('form_content')
+    @include('vendors.bookings.form')
+@endsection
+@push('scripts')
+
+@endpush
